7ecbcb8427c2f29099f6cb8fe7308d8f93d7ace9
rizkimaulana51 <rizkimaw51@gmail.com>
2021-08-29 08:28:38 +0700

    membuat project todolist

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+let container = document.querySelector(".container");
+let inputField = document.querySelector(".inputField");
+let addToDo = document.querySelector(".addToDo");
+let todos = document.querySelector(".todos");
+   
+addToDo.addEventListener("click", function() {
+			var p = document.createElement("p");
+			var btn = document.createElement("button");
+			p.classList.add("styling");
+			p.innerText = "• "+inputField.value;
+			todos.appendChild(p);
+			inputField.value = "";
+			p.addEventListener("click", function() {
+			p.classList.toggle("garis");
+			})
+});
 
 
